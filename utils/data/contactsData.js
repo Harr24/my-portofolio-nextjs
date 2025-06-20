33a25c57da0e807d563856f0b95b780ce80f32aa
Harr24 <137282048+Harr24@users.noreply.github.com>
2025-06-20 23:05:27 +0700

    Update ContactsData

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    email: 'Yusufharrysetiawan@gmail.com',
+    phone: '081326740142',
+    address: 'Kalimulya, Cilodong, Depok,',
+    github: 'https://github.com/Harr24',
+    facebook: 'https://www.facebook.com/Harry/',
+    linkedIn: 'https://www.linkedin.com/in/yusuf-harry/',
     twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    stackOverflow: 'https://stackoverflow.com/users/16840768/harry',//belom
+    devUsername: "harry"
 }
