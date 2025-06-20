05c51694e3bb92c9797f8d2a18cd3e5efbb2ec31
Harr24 <137282048+Harr24@users.noreply.github.com>
2025-06-20 22:47:38 +0700

    Add PKL experience entry

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
+    title: "Praktik Kerja Lapangan (PKL)",
+    company: "Naraya Computer",
+    duration: "(Feb 2021 – Apr 2021)",
+  },
+  {
+    id: 2,
     title: 'Software Engineer I',
     company: "Teton Private Ltd.",
     duration: "(Jan 2022 - Present)"
   },
   {
-    id: 2,
+    id: 3,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
     duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
   }
 ]
